a95ba8505c9305780fa64eab32e9f5df5beb385d
Russell Brabers <brabers.russell@gmail.com>
2021-02-27 22:57:58 -0500

    UI Updates
    - Bugs approaching and leaving the lights, or the light changing color, updates the UI count

--- Assets/_Scripts/BugManager.cs
+++ Assets/_Scripts/BugManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public struct BugPit
@@ -23,6 +25,10 @@ public class BugManager : MonoBehaviour
     public GameObject bugPrefab;
     public Material[] materials;
 
+    [Header("UI Elements")]
+    public GameObject[] panels;
+    [SerializeField]
+    private Sprite[] colorIcon;
 
     public static BugManager instance;
 
@@ -41,7 +47,7 @@ public class BugManager : MonoBehaviour
 
         InitiatePits();
 
-        // TODO: set up UI
+        InitializeUI();
 
         SpawnBugs();
     }
@@ -52,10 +58,36 @@ public class BugManager : MonoBehaviour
         {
             BugPit pit;
             pit.colorType = (ColorType)Random.Range(0, (int)ColorType.COUNT);
+
+            // Make sure you get three different colors
+            if(i == 1)
+            {
+                while(pit.colorType == bugPits[0].colorType)
+                {
+                    pit.colorType = (ColorType)Random.Range(0, (int)ColorType.COUNT);
+                }
+            }
+            else if(i == 2)
+            {
+                while (pit.colorType == bugPits[0].colorType || pit.colorType == bugPits[1].colorType)
+                {
+                    pit.colorType = (ColorType)Random.Range(0, (int)ColorType.COUNT);
+                }
+            }
+
             pit.maxCount = Random.Range(10, 16);
             pit.currentCount = 0;
 
             bugPits[i] = pit;
+        }
+    }
+
+    private void InitializeUI()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            panels[i].transform.GetChild(0).GetComponent<Image>().sprite = colorIcon[(int)bugPits[i].colorType];
+            panels[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = bugPits[i].currentCount.ToString("00") + " / " + bugPits[i].maxCount;
         }
     }
 
@@ -74,6 +106,31 @@ public class BugManager : MonoBehaviour
 
                 bug.GetComponent<BugBehaviour>().colorType = bp.colorType;
             }
+        }
+    }
+
+    public void IncrementBugCount(ColorType color, int count)
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if (bugPits[i].colorType != color)
+                continue;
+
+            bugPits[i].currentCount += count;
+            bugPits[i].currentCount = bugPits[i].currentCount > bugPits[i].maxCount ? bugPits[i].maxCount : bugPits[i].currentCount;
+            panels[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = bugPits[i].currentCount.ToString("00") + " / " + bugPits[i].maxCount;
+        }
+    }
+
+    public void SetBugCount(ColorType color, int value)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (bugPits[i].colorType != color)
+                continue;
+
+            bugPits[i].currentCount = value;
+            panels[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = bugPits[i].currentCount.ToString("00") + " / " + bugPits[i].maxCount;
         }
     }
 }
--- Assets/_Scripts/LightPostBehaviour.cs
+++ Assets/_Scripts/LightPostBehaviour.cs
@@ -3,24 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+public struct ColorCount
+{
+    public ColorType colorType;
+    public int count;
+}
+
 public class LightPostBehaviour : MonoBehaviour
 {
     public UnityEvent<GameObject> lightActivatedEvent;
 
-    [Header("Light Post Parts")]
+    [Header("Post Parts")]
     public GameObject[] pieces;
     public Light pointLight;
     public ParticleSystem particles;
 
+    [Header("ColorType Values")]
     public ColorType colorType;
+    public ColorCount[] bugCounts;
+
+    private void Start()
+    {
+        bugCounts = new ColorCount[(int)ColorType.COUNT];
+
+        for(int i = 0; i < (int)ColorType.COUNT; i++)
+        {
+            ColorCount count;
+            count.colorType = (ColorType)i;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Orb"))
+        if (other.gameObject.CompareTag("Orb"))
         {
             OrbBehaviour behaviour = other.GetComponent<OrbBehaviour>();
             MeshRenderer renderer = other.GetComponent<MeshRenderer>();
-            
+
+            if(colorType != ColorType.COUNT)
+            {
+                BugManager.instance.SetBugCount(colorType, 0);
+                
+                foreach (ColorCount cc in bugCounts)
+                {
+                    if(cc.colorType == behaviour.colorType)
+                    {
+                        for (int i = 0; i < cc.count; i++)
+                            BugManager.instance.IncrementBugCount(colorType, 1);
+                    }
+                }
+            }
+
             colorType = behaviour.colorType;
 
             foreach (GameObject p in pieces)
@@ -42,6 +75,53 @@ public class LightPostBehaviour : MonoBehaviour
                 OrbManager.instance.ResetOrb(other.gameObject);
 
             lightActivatedEvent.Invoke(gameObject);
+        }
+        else if (other.gameObject.CompareTag("Bug"))
+        {
+            if (other.GetComponent<BugBehaviour>().colorType == colorType)
+                BugManager.instance.IncrementBugCount(colorType, 1);
+            else
+                BugManager.instance.IncrementBugCount(colorType, -1);
+
+            for (int i = 0; i < bugCounts.Length; i++)
+            {
+                if (bugCounts[i].colorType != other.GetComponent<BugBehaviour>().colorType)
+                    continue;
+
+                bugCounts[i].count++;
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bug"))
+        {
+            if (other.GetComponent<BugBehaviour>().colorType == colorType)
+                BugManager.instance.IncrementBugCount(colorType, -1);
+            else
+                BugManager.instance.IncrementBugCount(colorType, 1);
+
+            for (int i = 0; i < bugCounts.Length; i++)
+            {
+                if (bugCounts[i].colorType != other.GetComponent<BugBehaviour>().colorType)
+                    continue;
+
+                bugCounts[i].count--;
+                break;
+            }
+        }
+    }
+
+    private void InformUI()
+    {
+        for (int i = 0; i < bugCounts.Length; i++)
+        {
+            if(bugCounts[i].colorType == colorType)
+                BugManager.instance.IncrementBugCount(colorType, bugCounts[i].count);
+            else
+                BugManager.instance.IncrementBugCount(colorType, -bugCounts[i].count);
         }
     }
 }
